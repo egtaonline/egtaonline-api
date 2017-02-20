17644778d162e107094817f81f1eb2db43294878
Erik Brinkman <erik.brinkman@gmail.com>
2017-02-20 00:34:13 -0500

    Remove setup.json

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
-import json
 import setuptools
 
 
-with open('setup.json') as f:
-    setuptools.setup(**json.load(f))
+setuptools.setup(
+  name="egtaonlineapi",
+  version="0.1",
+  description="Various APIs for egtaonline",
+  url="https://github.com/egtaonline/egtaonline-api.git",
+  author="Strategic Reasoning Group",
+  license="MIT",
+  entry_points=dict(console_scripts=["eo=egtaonline.eo:main"]),
+  install_requires=[
+    "lxml~=3.6",
+    "requests~=2.11",
+    "tabulate~=0.7",
+    "inflection~=0.3"
+  ],
+  packages=[
+    "egtaonline"
+  ]
+)
