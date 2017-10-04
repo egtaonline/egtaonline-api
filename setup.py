79109ec8ec518dd1c6259cff2a98858bd069044a
Erik Brinkman <erik.brinkman@gmail.com>
2017-10-04 10:48:58 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='egtaonlineapi',
-    version='0.1.0',
+    version='0.1.1',
     description='Various APIs for egtaonline',
     url='https://github.com/egtaonline/egtaonline-api.git',
     author='Strategic Reasoning Group',
