05498500cdfb32bfb8ed0acf3986636f9f4ce9ca
Erik Brinkman <erik.brinkman@gmail.com>
2017-10-17 01:34:33 -0400

    Move eo module to __main__

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     author='Strategic Reasoning Group',
     author_email='strategic.reasoning.group@umich.edu',
     license='Apache 2.0',
-    entry_points=dict(console_scripts=['eo=egtaonline.eo:main']),
+    entry_points=dict(console_scripts=['eo=egtaonline.__main__:main']),
     install_requires=[
         'lxml~=3.6',
         'requests~=2.11',
